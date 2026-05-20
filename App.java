f37057f5799c7c7bfdcbe3e26fc4013822908067
FELIPE BELOMI DE OLIVEIRA CAMPOS <felipe.campos003@cs.up.edu.br>
2026-05-20 20:59:17 -0300

    adicionei o segundo arraylist e mais alguns métodos

--- App.java
+++ App.java
@@ -13,7 +13,30 @@ public class App {
         System.out.println("Inteiros antes: " + inteiros);
         Collections.sort(inteiros);
         System.out.println("Inteiros depois: " + inteiros);
+        System.out.println("Collections.binarySearch(10) : " + Collections.binarySearch(inteiros, 10));
+        System.out.println("Collections.binarySearch(20) : " + Collections.binarySearch(inteiros, 20));
+
+        // 30 52 20 -10 100
+        ArrayList<Integer> inteiros2 = new ArrayList<>();
+        inteiros2.add(30);
+        inteiros2.add(52);
+        inteiros2.add(20);
+        inteiros2.add(-10);
+        inteiros2.add(100);
+
+        System.out.println("disjoint: " + ((Collections.disjoint(inteiros, inteiros2)) ? "tem elementos em comum"
+                : "não tem elementos em comum"));
+
+        inteiros2.add(100);
+
+        System.out.println(
+                "Exibindo frequência do valor 100 no segundo arraylist: " + Collections.frequency(inteiros2, 100)); // frequência
+                                                                                                                    // na
+        // qual o número
+        // 100 aparece
+        // no arraylist
 
         
+
     }
 }
