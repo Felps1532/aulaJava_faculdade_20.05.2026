84818ea8fd68679c4c9d3b12f3932e06fec06bd8
FELIPE BELOMI DE OLIVEIRA CAMPOS <felipe.campos003@cs.up.edu.br>
2026-05-25 19:27:49 -0300

    App3.java criado

---
+++ App3.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class App3 {
+    public static void main(String[] args) {
+        ArrayList<Pessoa> pessoas = new ArrayList<>();
+
+        pessoas.add(new Pessoa("Maria da Silva", "12345678912", "24/03/1290"));
+        pessoas.add(new Pessoa("Mariana Moraes", "11343671912", "23/09/1310"));
+        pessoas.add(new Pessoa("Maria da Silva", "21345678912", "12/12/1230"));
+    }
+}
